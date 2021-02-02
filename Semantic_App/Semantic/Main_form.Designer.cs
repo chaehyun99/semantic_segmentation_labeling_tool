0b22f6f9a131db1d3f77e85b38a0210ec3f2d74c
Jeon SeongWoon <64198101+dnstjdwjs@users.noreply.github.com>
2021-02-02 16:30:45 +0900

    Revert "합치는 중... 임시저장"
    This reverts commit 2f65a9d5de44c74f7ee231bd97d657248454ecb1.

--- Semantic_App/Semantic/Main_form.Designer.cs
+++ Semantic_App/Semantic/Main_form.Designer.cs
@@ -43,19 +43,18 @@ namespace Semantic
             this.btn_zoomReset = new System.Windows.Forms.Button();
             this.btn_brushSizeUp = new System.Windows.Forms.Button();
             this.btn_brushSizeDown = new System.Windows.Forms.Button();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // Network_operation
             // 
-            this.Network_operation.Location = new System.Drawing.Point(440, 87);
+            this.Network_operation.Location = new System.Drawing.Point(308, 58);
+            this.Network_operation.Margin = new System.Windows.Forms.Padding(2);
             this.Network_operation.Name = "Network_operation";
-            this.Network_operation.Size = new System.Drawing.Size(139, 134);
+            this.Network_operation.Size = new System.Drawing.Size(97, 89);
             this.Network_operation.TabIndex = 16;
             this.Network_operation.Text = "시멘틱 구동";
             this.Network_operation.UseVisualStyleBackColor = true;
@@ -65,9 +64,10 @@ namespace Semantic
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(440, 255);
+            this.pictureBox1.Location = new System.Drawing.Point(308, 170);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1245, 563);
+            this.pictureBox1.Size = new System.Drawing.Size(872, 376);
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -75,9 +75,10 @@ namespace Semantic
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(623, 87);
+            this.button2.Location = new System.Drawing.Point(436, 58);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 134);
+            this.button2.Size = new System.Drawing.Size(102, 89);
             this.button2.TabIndex = 18;
             this.button2.Text = "이미지 저장";
             this.button2.UseVisualStyleBackColor = true;
@@ -86,28 +87,28 @@ namespace Semantic
             // uiPanelThumbnail
             // 
             this.uiPanelThumbnail.AutoScroll = true;
-            this.uiPanelThumbnail.Location = new System.Drawing.Point(36, 87);
-            this.uiPanelThumbnail.Margin = new System.Windows.Forms.Padding(4);
+            this.uiPanelThumbnail.Location = new System.Drawing.Point(25, 58);
             this.uiPanelThumbnail.Name = "uiPanelThumbnail";
-            this.uiPanelThumbnail.Size = new System.Drawing.Size(381, 938);
+            this.uiPanelThumbnail.Size = new System.Drawing.Size(267, 625);
             this.uiPanelThumbnail.TabIndex = 19;
+            this.uiPanelThumbnail.Paint += new System.Windows.Forms.PaintEventHandler(this.uiPanelThumbnail_Paint);
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.경로설정FToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1871, 33);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(1310, 24);
             this.menuStrip1.TabIndex = 20;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // 경로설정FToolStripMenuItem
             // 
             this.경로설정FToolStripMenuItem.Name = "경로설정FToolStripMenuItem";
-            this.경로설정FToolStripMenuItem.Size = new System.Drawing.Size(125, 29);
+            this.경로설정FToolStripMenuItem.Size = new System.Drawing.Size(85, 22);
             this.경로설정FToolStripMenuItem.Text = "경로 설정(F)";
             this.경로설정FToolStripMenuItem.Click += new System.EventHandler(this.경로설정FToolStripMenuItem_Click);
             // 
@@ -115,9 +116,10 @@ namespace Semantic
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.Info;
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(547, 208);
+            this.pictureBox2.Location = new System.Drawing.Point(308, 170);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1245, 641);
+            this.pictureBox2.Size = new System.Drawing.Size(872, 376);
             this.pictureBox2.TabIndex = 23;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
@@ -127,28 +129,25 @@ namespace Semantic
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(949, 153);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Location = new System.Drawing.Point(664, 102);
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(149, 69);
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
             this.trackBar1.TabIndex = 24;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // percenttextBox
             // 
-            this.percenttextBox.Location = new System.Drawing.Point(797, 189);
-            this.percenttextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.percenttextBox.Location = new System.Drawing.Point(558, 126);
             this.percenttextBox.Name = "percenttextBox";
-            this.percenttextBox.Size = new System.Drawing.Size(141, 28);
+            this.percenttextBox.Size = new System.Drawing.Size(100, 21);
             this.percenttextBox.TabIndex = 25;
             this.percenttextBox.TextChanged += new System.EventHandler(this.percenttextBox_TextChanged);
             // 
             // btn_zoomIn
             // 
-            this.btn_zoomIn.Location = new System.Drawing.Point(797, 87);
-            this.btn_zoomIn.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_zoomIn.Location = new System.Drawing.Point(558, 58);
             this.btn_zoomIn.Name = "btn_zoomIn";
-            this.btn_zoomIn.Size = new System.Drawing.Size(61, 34);
+            this.btn_zoomIn.Size = new System.Drawing.Size(43, 23);
             this.btn_zoomIn.TabIndex = 26;
             this.btn_zoomIn.Text = "+";
             this.btn_zoomIn.UseVisualStyleBackColor = true;
@@ -156,10 +155,9 @@ namespace Semantic
             // 
             // btn_zoomOut
             // 
-            this.btn_zoomOut.Location = new System.Drawing.Point(937, 87);
-            this.btn_zoomOut.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_zoomOut.Location = new System.Drawing.Point(656, 58);
             this.btn_zoomOut.Name = "btn_zoomOut";
-            this.btn_zoomOut.Size = new System.Drawing.Size(61, 34);
+            this.btn_zoomOut.Size = new System.Drawing.Size(43, 23);
             this.btn_zoomOut.TabIndex = 27;
             this.btn_zoomOut.Text = "-";
             this.btn_zoomOut.UseVisualStyleBackColor = true;
@@ -167,10 +165,9 @@ namespace Semantic
             // 
             // btn_zoomReset
             // 
-            this.btn_zoomReset.Location = new System.Drawing.Point(867, 87);
-            this.btn_zoomReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_zoomReset.Location = new System.Drawing.Point(607, 58);
             this.btn_zoomReset.Name = "btn_zoomReset";
-            this.btn_zoomReset.Size = new System.Drawing.Size(61, 34);
+            this.btn_zoomReset.Size = new System.Drawing.Size(43, 23);
             this.btn_zoomReset.TabIndex = 28;
             this.btn_zoomReset.Text = "100%";
             this.btn_zoomReset.UseVisualStyleBackColor = true;
@@ -178,10 +175,9 @@ namespace Semantic
             // 
             // btn_brushSizeUp
             // 
-            this.btn_brushSizeUp.Location = new System.Drawing.Point(1129, 87);
-            this.btn_brushSizeUp.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_brushSizeUp.Location = new System.Drawing.Point(790, 58);
             this.btn_brushSizeUp.Name = "btn_brushSizeUp";
-            this.btn_brushSizeUp.Size = new System.Drawing.Size(107, 34);
+            this.btn_brushSizeUp.Size = new System.Drawing.Size(75, 23);
             this.btn_brushSizeUp.TabIndex = 29;
             this.btn_brushSizeUp.Text = "Brush +";
             this.btn_brushSizeUp.UseVisualStyleBackColor = true;
@@ -189,34 +185,19 @@ namespace Semantic
             // 
             // btn_brushSizeDown
             // 
-            this.btn_brushSizeDown.Location = new System.Drawing.Point(1129, 130);
-            this.btn_brushSizeDown.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_brushSizeDown.Location = new System.Drawing.Point(790, 87);
             this.btn_brushSizeDown.Name = "btn_brushSizeDown";
-            this.btn_brushSizeDown.Size = new System.Drawing.Size(107, 34);
+            this.btn_brushSizeDown.Size = new System.Drawing.Size(75, 23);
             this.btn_brushSizeDown.TabIndex = 30;
             this.btn_brushSizeDown.Text = "Brush -";
             this.btn_brushSizeDown.UseVisualStyleBackColor = true;
             this.btn_brushSizeDown.Click += new System.EventHandler(this.btn_brushSizeDown_Click);
             // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.Info;
-            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox3.Location = new System.Drawing.Point(691, 153);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(1245, 563);
-            this.pictureBox3.TabIndex = 31;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
-            this.pictureBox3.MouseEnter += new System.EventHandler(this.pictureBox3_MouseEnter);
-            this.pictureBox3.MouseLeave += new System.EventHandler(this.pictureBox3_MouseLeave);
-            // 
             // Main_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1871, 1050);
-            this.Controls.Add(this.pictureBox3);
+            this.ClientSize = new System.Drawing.Size(1310, 700);
             this.Controls.Add(this.btn_brushSizeDown);
             this.Controls.Add(this.btn_brushSizeUp);
             this.Controls.Add(this.btn_zoomReset);
@@ -231,7 +212,6 @@ namespace Semantic
             this.Controls.Add(this.Network_operation);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Main_form";
             this.Text = "Main_form";
             this.Load += new System.EventHandler(this.Main_form_Load);
@@ -241,7 +221,6 @@ namespace Semantic
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -263,7 +242,6 @@ namespace Semantic
         private System.Windows.Forms.Button btn_zoomReset;
         private System.Windows.Forms.Button btn_brushSizeUp;
         private System.Windows.Forms.Button btn_brushSizeDown;
-        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
