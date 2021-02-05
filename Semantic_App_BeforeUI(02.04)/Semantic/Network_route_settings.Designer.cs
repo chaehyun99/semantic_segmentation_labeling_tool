936180439ff797489b809895760492c4971872ad
Jeon SeongWoon <64198101+dnstjdwjs@users.noreply.github.com>
2021-02-06 08:26:55 +0900

    Update Network 경로설정 form UI

--- Semantic_App_BeforeUI(02.04)/Semantic/Network_route_settings.Designer.cs
+++ Semantic_App_BeforeUI(02.04)/Semantic/Network_route_settings.Designer.cs
@@ -40,120 +40,173 @@ namespace Semantic
             this.label3 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.Save = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Input_image_path
             // 
-            this.Input_image_path.Location = new System.Drawing.Point(222, 73);
-            this.Input_image_path.Margin = new System.Windows.Forms.Padding(2);
+            this.Input_image_path.Location = new System.Drawing.Point(178, 160);
+            this.Input_image_path.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Input_image_path.Name = "Input_image_path";
-            this.Input_image_path.Size = new System.Drawing.Size(414, 21);
+            this.Input_image_path.Size = new System.Drawing.Size(285, 21);
             this.Input_image_path.TabIndex = 10;
             // 
             // Gray_scale_path
             // 
-            this.Gray_scale_path.Location = new System.Drawing.Point(222, 177);
-            this.Gray_scale_path.Margin = new System.Windows.Forms.Padding(2);
+            this.Gray_scale_path.Location = new System.Drawing.Point(178, 97);
+            this.Gray_scale_path.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Gray_scale_path.Name = "Gray_scale_path";
-            this.Gray_scale_path.Size = new System.Drawing.Size(414, 21);
+            this.Gray_scale_path.Size = new System.Drawing.Size(285, 21);
             this.Gray_scale_path.TabIndex = 11;
             // 
             // Rgb_path
             // 
-            this.Rgb_path.Location = new System.Drawing.Point(222, 284);
-            this.Rgb_path.Margin = new System.Windows.Forms.Padding(2);
+            this.Rgb_path.Location = new System.Drawing.Point(178, 223);
+            this.Rgb_path.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Rgb_path.Name = "Rgb_path";
-            this.Rgb_path.Size = new System.Drawing.Size(414, 21);
+            this.Rgb_path.Size = new System.Drawing.Size(285, 21);
             this.Rgb_path.TabIndex = 12;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(673, 73);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button1.Location = new System.Drawing.Point(475, 97);
+            this.button1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 13;
             this.button1.Text = "찾아보기";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(673, 177);
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button2.Location = new System.Drawing.Point(475, 159);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 14;
             this.button2.Text = "찾아보기";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(673, 287);
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button3.Location = new System.Drawing.Point(475, 222);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 15;
             this.button3.Text = "찾아보기";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 15F);
-            this.label1.Location = new System.Drawing.Point(4, 73);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label1.Location = new System.Drawing.Point(80, 99);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.Size = new System.Drawing.Size(91, 16);
             this.label1.TabIndex = 16;
             this.label1.Text = "입력 이미지 경로";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 15F);
-            this.label2.Location = new System.Drawing.Point(4, 178);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label2.Location = new System.Drawing.Point(59, 162);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(176, 20);
+            this.label2.Size = new System.Drawing.Size(112, 16);
             this.label2.TabIndex = 17;
             this.label2.Text = "Grayscal 저장 경로";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("굴림", 15F);
-            this.label3.Location = new System.Drawing.Point(4, 285);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label3.Location = new System.Drawing.Point(51, 225);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(202, 20);
+            this.label3.Size = new System.Drawing.Size(120, 16);
             this.label3.TabIndex = 18;
             this.label3.Text = "Rgb 이미지 저장 경로";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(244, 360);
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(214, 300);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 42);
+            this.button4.Size = new System.Drawing.Size(104, 40);
             this.button4.TabIndex = 19;
             this.button4.Text = "취소";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Save
             // 
-            this.Save.Location = new System.Drawing.Point(479, 360);
+            this.Save.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Location = new System.Drawing.Point(328, 300);
             this.Save.Name = "Save";
-            this.Save.Size = new System.Drawing.Size(105, 42);
+            this.Save.Size = new System.Drawing.Size(104, 40);
             this.Save.TabIndex = 20;
             this.Save.Text = "저장";
-            this.Save.UseVisualStyleBackColor = true;
+            this.Save.UseVisualStyleBackColor = false;
             this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 45);
+            this.panel1.TabIndex = 21;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
+            this.button5.BackgroundImage = global::Semantic.Properties.Resources.iconfinder_Folder_Place_File_Storage_Paper_Office_1343439;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(15, 6);
+            this.button5.Margin = new System.Windows.Forms.Padding(7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(29, 30);
+            this.button5.TabIndex = 34;
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // Network_route_settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(628, 388);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.Save);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
@@ -169,6 +222,7 @@ namespace Semantic
             this.ShowInTaskbar = false;
             this.Text = "Network_route_settings";
             this.Load += new System.EventHandler(this.Network_route_settings_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,5 +241,7 @@ namespace Semantic
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
     }
 }
