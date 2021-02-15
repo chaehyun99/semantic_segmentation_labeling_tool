fdb07900df57894c99c425410cdce3395f13cbb8
Jeon SeongWoon <64198101+dnstjdwjs@users.noreply.github.com>
2021-02-15 23:38:54 +0900

    Add 불러오기/저장 경로설정 폼 UI

---
+++ Semantic_App_BeforeUI(02.04)/Semantic/Network_route_settings.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Semantic
+{
+    partial class Network_route_settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input_image_path = new System.Windows.Forms.TextBox();
+            this.Gray_scale_path = new System.Windows.Forms.TextBox();
+            this.Rgb_path = new System.Windows.Forms.TextBox();
+            this.button_Findpath_Input = new System.Windows.Forms.Button();
+            this.button_Findpath_Gray = new System.Windows.Forms.Button();
+            this.button_Findpath_Rgb = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Input_image_path
+            // 
+            this.Input_image_path.Location = new System.Drawing.Point(178, 99);
+            this.Input_image_path.Margin = new System.Windows.Forms.Padding(1);
+            this.Input_image_path.Name = "Input_image_path";
+            this.Input_image_path.Size = new System.Drawing.Size(285, 21);
+            this.Input_image_path.TabIndex = 10;
+            // 
+            // Gray_scale_path
+            // 
+            this.Gray_scale_path.Location = new System.Drawing.Point(178, 161);
+            this.Gray_scale_path.Margin = new System.Windows.Forms.Padding(1);
+            this.Gray_scale_path.Name = "Gray_scale_path";
+            this.Gray_scale_path.Size = new System.Drawing.Size(285, 21);
+            this.Gray_scale_path.TabIndex = 11;
+            // 
+            // Rgb_path
+            // 
+            this.Rgb_path.Location = new System.Drawing.Point(178, 223);
+            this.Rgb_path.Margin = new System.Windows.Forms.Padding(1);
+            this.Rgb_path.Name = "Rgb_path";
+            this.Rgb_path.Size = new System.Drawing.Size(285, 21);
+            this.Rgb_path.TabIndex = 12;
+            // 
+            // button_Findpath_Input
+            // 
+            this.button_Findpath_Input.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button_Findpath_Input.FlatAppearance.BorderSize = 0;
+            this.button_Findpath_Input.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Findpath_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button_Findpath_Input.Location = new System.Drawing.Point(475, 97);
+            this.button_Findpath_Input.Margin = new System.Windows.Forms.Padding(1);
+            this.button_Findpath_Input.Name = "button_Findpath_Input";
+            this.button_Findpath_Input.Size = new System.Drawing.Size(75, 23);
+            this.button_Findpath_Input.TabIndex = 13;
+            this.button_Findpath_Input.Text = "찾아보기";
+            this.button_Findpath_Input.UseVisualStyleBackColor = false;
+            this.button_Findpath_Input.Click += new System.EventHandler(this.button_Findpath_Input_Click);
+            // 
+            // button_Findpath_Gray
+            // 
+            this.button_Findpath_Gray.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button_Findpath_Gray.FlatAppearance.BorderSize = 0;
+            this.button_Findpath_Gray.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Findpath_Gray.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button_Findpath_Gray.Location = new System.Drawing.Point(475, 159);
+            this.button_Findpath_Gray.Margin = new System.Windows.Forms.Padding(2);
+            this.button_Findpath_Gray.Name = "button_Findpath_Gray";
+            this.button_Findpath_Gray.Size = new System.Drawing.Size(75, 23);
+            this.button_Findpath_Gray.TabIndex = 14;
+            this.button_Findpath_Gray.Text = "찾아보기";
+            this.button_Findpath_Gray.UseVisualStyleBackColor = false;
+            this.button_Findpath_Gray.Click += new System.EventHandler(this.button_Findpath_Gray_Click);
+            // 
+            // button_Findpath_Rgb
+            // 
+            this.button_Findpath_Rgb.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button_Findpath_Rgb.FlatAppearance.BorderSize = 0;
+            this.button_Findpath_Rgb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Findpath_Rgb.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button_Findpath_Rgb.Location = new System.Drawing.Point(475, 222);
+            this.button_Findpath_Rgb.Margin = new System.Windows.Forms.Padding(2);
+            this.button_Findpath_Rgb.Name = "button_Findpath_Rgb";
+            this.button_Findpath_Rgb.Size = new System.Drawing.Size(75, 23);
+            this.button_Findpath_Rgb.TabIndex = 15;
+            this.button_Findpath_Rgb.Text = "찾아보기";
+            this.button_Findpath_Rgb.UseVisualStyleBackColor = false;
+            this.button_Findpath_Rgb.Click += new System.EventHandler(this.button_Findpath_Rgb_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label1.Location = new System.Drawing.Point(80, 99);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "입력 이미지 경로";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label2.Location = new System.Drawing.Point(59, 162);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Grayscal 저장 경로";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label3.Location = new System.Drawing.Point(51, 225);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Rgb 이미지 저장 경로";
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button_cancel.FlatAppearance.BorderSize = 0;
+            this.button_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_cancel.Location = new System.Drawing.Point(214, 300);
+            this.button_cancel.Margin = new System.Windows.Forms.Padding(2);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(104, 40);
+            this.button_cancel.TabIndex = 19;
+            this.button_cancel.Text = "취소";
+            this.button_cancel.UseVisualStyleBackColor = false;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Save.FlatAppearance.BorderSize = 0;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Location = new System.Drawing.Point(328, 300);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(104, 40);
+            this.Save.TabIndex = 20;
+            this.Save.Text = "저장";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(628, 45);
+            this.panel1.TabIndex = 21;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(63)))), ((int)(((byte)(80)))));
+            this.button5.BackgroundImage = global::Semantic.Properties.Resources.iconfinder_Folder_Place_File_Storage_Paper_Office_1343439;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(15, 6);
+            this.button5.Margin = new System.Windows.Forms.Padding(7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(29, 30);
+            this.button5.TabIndex = 34;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // Network_route_settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(628, 388);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Findpath_Rgb);
+            this.Controls.Add(this.button_Findpath_Gray);
+            this.Controls.Add(this.button_Findpath_Input);
+            this.Controls.Add(this.Rgb_path);
+            this.Controls.Add(this.Gray_scale_path);
+            this.Controls.Add(this.Input_image_path);
+            this.Name = "Network_route_settings";
+            this.ShowInTaskbar = false;
+            this.Text = "Network_route_settings";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_image_path;
+        private System.Windows.Forms.TextBox Gray_scale_path;
+        private System.Windows.Forms.TextBox Rgb_path;
+        private System.Windows.Forms.Button button_Findpath_Input;
+        private System.Windows.Forms.Button button_Findpath_Gray;
+        private System.Windows.Forms.Button button_Findpath_Rgb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ Semantic_App_BeforeUI(02.04)/Semantic/Network_route_settings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semantic
+{
+    public partial class Network_route_settings : Form
+    {
+        public Network_route_settings()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Findpath_Input_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Findpath_Gray_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Findpath_Rgb_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
